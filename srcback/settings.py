02c290baa7a2b4b0f285903e4cc712cb618dba5c
fabianunivalle <benavides.fabian@correounivalle.edu.co>
2020-03-29 13:39:46 -0500

    se crean las tablas para la base de datos

--- srcback/settings.py
+++ srcback/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'factura',
     
      'rest_framework',
      'apps.publicidad',
@@ -78,8 +79,12 @@ WSGI_APPLICATION = 'srcback.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'wattlab',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'wattlab.cjbpeuvptazq.us-east-2.rds.amazonaws.com',
+        'PORT': '5432', 
     }
 }
 
