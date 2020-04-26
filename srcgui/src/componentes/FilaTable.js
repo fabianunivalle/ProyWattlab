65d843c856884a81d36262f09923ebc719964a89
juancardona97 <juan.david.castro@correounivalle.edu.co>
2020-04-25 22:48:31 -0500

    Se añadio botones que dependiendo del estado del usuario o fila cambia su tipo

--- srcgui/src/componentes/FilaTable.js
+++ srcgui/src/componentes/FilaTable.js
@@ -54,6 +54,39 @@ function FilaTable (props){
         }
     }
 
+    const mostrarBotonA = () =>{
+        if(props.dato4==='ramiro.info'){
+            return(
+                <td>         
+                    <button type="button" className="btn btn-danger" style={{marginBottom: "10px", width: "130px"}} >
+                        <svg className="bi bi-eye-slash-fill" width="20px" height="20px" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10.79 12.912l-1.614-1.615a3.5 3.5 0 01-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 002.79-.588zM5.21 3.088A7.028 7.028 0 018 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 00-4.474-4.474L5.21 3.089z"/>
+                            <path d="M5.525 7.646a2.5 2.5 0 002.829 2.829l-2.83-2.829zm4.95.708l-2.829-2.83a2.5 2.5 0 012.829 2.829z"/>
+                            <path fill-rule="evenodd" d="M13.646 14.354l-12-12 .708-.708 12 12-.708.708z" clip-rule="evenodd"/>
+                        </svg>
+                        &nbsp; Inactivar
+                    </button>       
+                </td>
+            )
+        }else{
+            return(
+                <td>
+                        
+                            
+                        <button type="button" className="btn btn-primary" style={{marginBottom: "10px", width: "130px"}} >
+                        <svg class="bi bi-eye-fill" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M10.5 8a2.5 2.5 0 11-5 0 2.5 2.5 0 015 0z"/>
+                            <path fill-rule="evenodd" d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 100-7 3.5 3.5 0 000 7z" clip-rule="evenodd"/>
+                        </svg>
+                            &nbsp; Activar
+                        </button>
+                        
+                    </td>
+            )
+        }
+
+    }
+
     /*Aqui pinta la fila, el id que es el pricipal, las celdas basicas, un icono, y los botones*/
     const mostrarFila= () =>{
         return(
@@ -76,16 +109,9 @@ function FilaTable (props){
                         </button>
                         &nbsp;
                     </td>
-                    <td>
-                        <button type="button" className="btn btn-danger" style={{marginBottom: "10px", width: "130px"}} >
-                            <svg className="bi bi-eye-slash-fill" width="20px" height="20px" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M10.79 12.912l-1.614-1.615a3.5 3.5 0 01-4.474-4.474l-2.06-2.06C.938 6.278 0 8 0 8s3 5.5 8 5.5a7.029 7.029 0 002.79-.588zM5.21 3.088A7.028 7.028 0 018 2.5c5 0 8 5.5 8 5.5s-.939 1.721-2.641 3.238l-2.062-2.062a3.5 3.5 0 00-4.474-4.474L5.21 3.089z"/>
-                                <path d="M5.525 7.646a2.5 2.5 0 002.829 2.829l-2.83-2.829zm4.95.708l-2.829-2.83a2.5 2.5 0 012.829 2.829z"/>
-                                <path fill-rule="evenodd" d="M13.646 14.354l-12-12 .708-.708 12 12-.708.708z" clip-rule="evenodd"/>
-                            </svg>
-                            &nbsp; Inactivar
-                        </button>
-                    </td>
+
+                    {mostrarBotonA()}
+                    
                 </tr>
             </React.Fragment>
         )
