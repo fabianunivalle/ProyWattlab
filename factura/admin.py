82b7bb59cf2dbc4fbc581e90fd6e11fddeb514f1
fabianunivalle <benavides.fabian@correounivalle.edu.co>
2020-05-01 08:04:40 -0500

    se realizan modificaciones solicitadas por el SM

--- factura/admin.py
+++ factura/admin.py
@@ -5,6 +5,6 @@ admin.site.register(Cliente)
 admin.site.register(SubEstacion)
 admin.site.register(Transformador)
 admin.site.register(Tarifa)
-admin.site.register(SucursalCliente)
+admin.site.register(Contrato)
 admin.site.register(Pago)
 # Register your models here.
--- factura/models.py
+++ factura/models.py
@@ -1,86 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-
-class ciudad(models.Model):
-    cnsctvo_dprtmnto = models.ForeignKey('departamento',on_delete=models.CASCADE)
-    nmbre_cdd = models.CharField(max_length=50)
-
-class departamento(models.Model):
-    nmbre_dprtmnto = models.CharField(max_length=150)
-
-class TipoUsuario(models.Model):
-    dscrpcn_tpo_usrio = models.CharField(max_length=50)
-
-class EstadoUsuario(models.Model):
-    dscrpcn_estdo_usro = models.CharField(max_length=50)
-
-class UsuarioEstadoVigencia(models.Model):
-    nmro_unco_idntfccn_usro = models.ForeignKey('Usuario',on_delete=models.CASCADE)
-    cnsctvo_estdo_usro = models.ForeignKey('EstadoUsuario',on_delete=models.CASCADE)
-    inco_vgnca = models.DateField()
-    fn_vgnca = models.DateField()
-    obsrvcn = models.CharField(max_length=150)
-
-class Usuario(models.Model):
-    nmbre_usro = models.CharField(max_length=50)
-    cnsctvo_tpo_usrio = models.ForeignKey('TipoUsuario',on_delete=models.CASCADE)
-    psswrd = models.CharField(max_length=50)
-
-class Pago(models.Model):
-    nmro_unco_idntfccn_bnco = models.ForeignKey('Banco',on_delete=models.CASCADE)
-    cnsctvo_fctra = models.ForeignKey('Facturacion',on_delete=models.CASCADE)
-    vlr_pgdo = models.FloatField()
-    nmro_unco_idntfccn_usro = models.ForeignKey('Usuario',on_delete=models.CASCADE)
-    fcha_pgo = models.DateField()
-
-class Banco(models.Model):
-    nmbre_bnco = models.CharField(max_length=50,default = '')
-    drccn = models.CharField(max_length=150,default = '')
-    tlfno = models.CharField(max_length=50,default = '')
-    cnsctvo_cdd = models.ForeignKey('ciudad',default = 1, on_delete=models.CASCADE)
-
-class TipoIdentificacion(models.Model):
-    dscrpcn_tpo_idntfcn = models.CharField(max_length=50)
-
-class Genero(models.Model):
-    dscrpcn_gnro = models.CharField(max_length=50)
-
-class tipoCliente(models.Model):
-    dscrpcn_tpo_clnte = models.CharField(max_length=50)
-
-class Cliente(models.Model):
-    cnsctvo_tpo_idntfcn = models.ForeignKey('TipoIdentificacion',on_delete=models.CASCADE)
-    nmro_idntfccn = models.CharField(max_length=50)
-    prmr_nmbre = models.CharField(max_length=50)
-    sgndo_nmbre = models.CharField(max_length=50)
-    prmr_aplldo = models.CharField(max_length=50)
-    sgndo_aplldo = models.CharField(max_length=50)
-    fcha_ncmnto = models.DateField()
-    cnsctvo_gnro = models.ForeignKey('Genero',on_delete=models.CASCADE)
-    cnsctvo_tpo_clnte = models.ForeignKey('tipoCliente',on_delete=models.CASCADE)
 
 class EstadoSucursalCliente(models.Model):
     dscrpcn_estdo_scrsl_clnte = models.CharField(max_length=50)
 
-class SucursalCliente(models.Model):
-    nmro_unco_idntfccn_clnte = models.ForeignKey('Cliente',on_delete=models.CASCADE)
-    rzn_scl_scrsl = models.CharField(max_length=150)
-    drccn = models.CharField(max_length=150)
-    tlfno = models.CharField(max_length=50)
-    cnsctvo_cdd = models.ForeignKey('ciudad',on_delete=models.CASCADE)
-    inco_vgnca = models.DateField()
-    fn_vgnca = models.DateField()
-    email = models.EmailField()
-    nmro_unco_idntfccn_trnsfrmdr = models.ForeignKey('Transformador',on_delete=models.CASCADE)
+class TipoIdentificacion(models.Model):
+    dscrpcn_tpo_idntfcn = models.CharField(max_length=50)
 
-class Consumo(models.Model):
-    cnsctvo_scrsl_clnte = models.ForeignKey('SucursalCliente',on_delete=models.CASCADE)
-    kwh = models.IntegerField()
-    prdo_cnsmo = models.IntegerField()
-
-class EstadoFactura(models.Model):
-    dscrpcn_estdo_usro = models.CharField(max_length=50)
+class tipoCliente(models.Model):
+    dscrpcn_tpo_clnte = models.CharField(max_length=50)
 
 class Tarifa(models.Model):
     vlr_kwh = models.FloatField()
@@ -88,19 +18,11 @@ class Tarifa(models.Model):
     fn_vgnca = models.DateField()
     obsrvcn = models.CharField(max_length=150)
 
-class Facturacion(models.Model):
-    cnsctvo_cnsmo = models.ForeignKey('Consumo',on_delete=models.CASCADE)
-    cnsctvo_trfa = models.FloatField()
-    vlr_cnsmo = models.FloatField()
-    vlr_intrss_mra = models.FloatField()
-    vlr_rcnxn = models.FloatField()
-    vlr_ttl = models.FloatField()
-    fcha_lmte_pgo = models.DateField()
-    cntdd_fctrs_pndts = models.IntegerField()
-    fcha_crte_srvco = models.DateField()
+class departamento(models.Model):
+    nmbre_dprtmnto = models.CharField(max_length=150)
 
 class Transformador(models.Model):
-    nmro_unco_idntfccn_sub_estcn = models.ForeignKey('SubEstacion',on_delete=models.CASCADE)
+    nmro_unco_idntfccn_sub_estcn = nmro_unco_idntfccn_sub_estcn = models.ForeignKey('SubEstacion',on_delete=models.CASCADE)
     tnsn_prmra = models.IntegerField()
     tnsn_mxma_srvco = models.IntegerField()
     tnsn_scndra = models.IntegerField()
@@ -112,11 +34,60 @@ class Transformador(models.Model):
     lngtd = models.IntegerField()
     lttd = models.IntegerField()
 
+class Pago(models.Model):
+    nmro_unco_idntfccn_bnco = models.ForeignKey('Banco',on_delete=models.CASCADE)
+    cnsctvo_fctra = models.OneToOneField('Facturacion',on_delete=models.CASCADE, unique = True)
+    vlr_pgdo = models.FloatField()
+    nmro_unco_idntfccn_usro = models.ForeignKey(User,on_delete=models.CASCADE)
+    fcha_pgo = models.DateField()
+
+class Banco(models.Model):
+    nmbre_bnco = models.CharField(max_length=50)
+    drccn = models.CharField(max_length=150)
+    tlfno = models.CharField(max_length=50)
+    cnsctvo_cdd = models.ForeignKey('ciudad',on_delete=models.CASCADE)
+
+class Cliente(models.Model):
+    cnsctvo_tpo_idntfcn = models.ForeignKey('TipoIdentificacion',on_delete=models.CASCADE)
+    nmro_idntfccn = models.CharField(max_length=50)
+    prmr_nmbre = models.CharField(max_length=50)
+    sgndo_nmbre = models.CharField(max_length=50)
+    prmr_aplldo = models.CharField(max_length=50)
+    sgndo_aplldo = models.CharField(max_length=50)
+    fcha_ncmnto = models.DateField()
+    cnsctvo_tpo_clnte = models.ForeignKey('tipoCliente',on_delete=models.CASCADE)
+
+class Contrato(models.Model):
+    nmro_unco_idntfccn_clnte = models.IntegerField()
+    estrt_scl = models.CharField(max_length=150)
+    drccn = models.CharField(max_length=150)
+    cnsctvo_cdd = models.ForeignKey('ciudad',on_delete=models.CASCADE)
+
+class Consumo(models.Model):
+    nmro_unco_idntfccn_cntrto = models.ForeignKey('Contrato',on_delete=models.CASCADE)
+    kwh = models.IntegerField()
+    prdo_cnsmo = models.IntegerField()
+
+class Facturacion(models.Model):
+    cnsctvo_cnsmo = models.ForeignKey('Consumo',on_delete=models.CASCADE)
+    cnsctvo_trfa = models.FloatField()
+    vlr_cnsmo = models.FloatField()
+    vlr_intrss_mra = models.FloatField()
+    vlr_rcnxn = models.FloatField()
+    vlr_ttl = models.FloatField()
+    fcha_lmte_pgo = models.DateField()
+    cntdd_fctrs_pndts = models.IntegerField()
+    fcha_crte_srvco = models.DateField()
+    estdo_fctra = models.BooleanField()
+
 class SubEstacion(models.Model):
-    nmro_unco_idntfccn = models.IntegerField(primary_key=True)
     nombre = models.CharField(max_length=50)
     drccn = models.CharField(max_length=150)
     tlfno = models.CharField(max_length=50)
     cnsctvo_cdd = models.ForeignKey('ciudad',on_delete=models.CASCADE)
     lngtd = models.IntegerField()
     lttd = models.IntegerField()
+
+class ciudad(models.Model):
+    cnsctvo_dprtmnto = models.ForeignKey('departamento',on_delete=models.CASCADE)
+    nmbre_cdd = models.CharField(max_length=50)
